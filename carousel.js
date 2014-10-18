64a1b42fee0d22f6f763d8f146ad1f1cb66a3374
Andrew Morris <voltrevo@gmail.com>
2014-10-18 23:31:21 +1100

    Configuration with defaults, change opacity based on z position, small y movement by default.

--- carousel.js
+++ carousel.js
@@ -5,16 +5,16 @@ var $
 var carousel = {}
 
 ;(function(){
-	function make3dPlacer(parentNode, frontMultiplier, backMultiplier)
+	function make3dPlacer(parentNode, frontScale, backScale)
 	{
-		var left = $(parentNode).width() / 7
-		var right = 6 * left;
-		var top = $(parentNode).height() / 4
-		var bottom = 3 * top
+		var left = 0
+		var right = $(parentNode).width();
+		var top = 0
+		var bottom = $(parentNode).height()
 
 		return function(node, originalDim, x, y, z)
 		{
-			var scale = frontMultiplier + (z + 1) / 2 * (backMultiplier - frontMultiplier)
+			var scale = backScale + (z + 1) / 2 * (frontScale - backScale)
 			node.style.width = scale * originalDim.width + 'px'
 			node.style.height = scale * originalDim.height + 'px'
 			node.style.left = left + (x + 1) / 2 * (right - left) - scale * originalDim.width / 2 + 'px'
@@ -22,8 +22,26 @@ var carousel = {}
 		}
 	}
 
-	carousel.create = function(carousel)
+	carousel.create = function(carousel, options)
 	{
+		// defaults
+		var opt = {
+			width: 0.7,
+			height: 0.15,
+			frontScale: 1,
+			backScale: 0.4,
+			opacity: {
+				min: 0.3,
+				max: 1,
+				threshold: -0.3
+			}
+		}
+
+		for (var key in options)
+		{
+			opt[key] = options[key]
+		}
+
 		var offset = 0
 		var originalDims = []
 		var mouseX = 0
@@ -45,25 +63,32 @@ var carousel = {}
 		setInterval(
 			function()
 			{
-				offset += 0.03 * mouseX * mouseX * mouseX
+				offset -= 0.03 * mouseX * mouseX * mouseX
 
-				var placer = make3dPlacer(carousel, 1, 0.2)
+				var placer = make3dPlacer(carousel, opt.frontScale, opt.backScale)
 				var children = $(carousel).children()
 				var childrenWithZ = []
 				$(carousel).children().each(function(index2, child)
 				{
 					var angle = index2 / children.length * 2 * Math.PI + offset
 
-					var zPos = Math.cos(angle)
-					placer(child, originalDims[index2], Math.sin(angle), 0, zPos)
+					var sin = Math.sin(angle)
+					var cos = Math.cos(angle)
+
+					var zPos = cos
+					placer(child, originalDims[index2], opt.width * sin, opt.height * cos, zPos)
+
 					childrenWithZ.push(
 						{
 							child: child,
 							z: zPos
 						})
+
+					var op = opt.opacity
+					child.style.opacity = op.min + (op.max - op.min) * (zPos > op.threshold ? 1 : (zPos + 1) / (op.threshold + 1))
 				})
 
-				childrenWithZ.sort(function(a, b) {return b.z - a.z})
+				childrenWithZ.sort(function(a, b) {return a.z - b.z})
 
 				for (var i in childrenWithZ)
 				{
